7acdd1b0ccef7982cd205b91b2da70b6bdcb6ce7
Daniel Augusto <danisson10@gmail.com>
2015-05-07 20:10:34 -0300

    Arrumando BackwardEuler

--- métodos/métodos.cpp
+++ métodos/métodos.cpp
@@ -1,6 +1,7 @@
 #include "métodos.h"
 #include <cassert>
 #include <cmath>
+#include <cstdio>
 
 double chp::ForwardEuler::operator()(double x) {
 	double y = y0;
@@ -14,14 +15,13 @@ double chp::BackwardEuler::operator()(double x) {
 	// (yn+1)^k = yn + step*f((yn+1)^k-1,x)
 	double y = y0;
 	for (double i = 0; fabs(x-i) >= step; i+=step)
-		y += chp::pontoFixo([&](double yn){return y+step*f(yn,i+step)},epsilon,y);
+		y = chp::pontoFixo([&](double yn){return y+step*f(yn,i+step);},epsilon,y);
 	return y;
 }
 
 double chp::pontoFixo(funcaoReal f,double epsilon, double x0) {
 	double x = x0;
-	while(fabs(f(x) - x) <= epsilon){
+	while(fabs(f(x) - x) >= epsilon)
 		x = f(x);
-	}
 	return f(x);
 }
