233db1f8fb2a1f977840324543b7984e3cafaa6c
Carol <carolhmj@gmail.com>
2015-05-07 22:11:18 +0000

    Modificação do main para usar o arquivo

--- visualização/main.py
+++ visualização/main.py
@@ -1,16 +1,30 @@
 #encoding=utf-8
 
+import sys
 import pygame
+import traceback
 from massa import *
 
 def lerArquivoAnimacao(nomeArquivo):
-	arquivo = open(nomeArquivo)
-	passo = linhasArquivo[0]
-	linhasArquivo = arquivo.readlines()
+	try:
+		arquivo = open(nomeArquivo)
+		passo = float(arquivo.readline())
+		
+		linhasArquivo = arquivo.readlines()
+		animacao = []
+		
+		for linha in linhasArquivo:
+			y = float(linha)
+			animacao.append([0,y])
 
-def main():
+		return passo, animacao
 
-	cores = {"VERMELHO": (255,0,0), "AZUL": (0,0,255), "VERDE": (0,255,0), "PRETO": (255,255,255), "BRANCO": (0,0,0)}
+	except Exception, e:
+		raise e
+
+def renderizar(passo, animacao):
+
+	cores = {"VERMELHO": (255,0,0), "AZUL": (0,0,255), "VERDE": (0,255,0), "PRETO": (0,0,0), "BRANCO": (255,255,255)}
 	tamanhoTela = (640,480)
 
 	pygame.init()
@@ -18,10 +32,9 @@ def main():
 	tela = pygame.display.set_mode(tamanhoTela)
 	pygame.display.set_caption("Visualização de Resolução de EDO")
 
-	passosAnimacao = [[0,10],[0,15],[0,20],[0,25],[0,30],[0,35],[0,40]]
 	passoAtual = 0
 
-	caixa1 = Massa(20,10,cores["VERDE"],passosAnimacao,tela)
+	caixa1 = Massa(20, 10, cores["VERDE"], animacao, tela)
 
 	aberto = True
 	relogio = pygame.time.Clock()
@@ -34,8 +47,19 @@ def main():
 			tela.fill(cores["BRANCO"])
 			caixa1.desenharProxPosicao()
 			pygame.display.flip()
-			relogio.tick(len(passosAnimacao))
+			relogio.tick(passo/10)
 
+def main():
+	nomeArquivo = sys.argv[1]
+	print(nomeArquivo)
+	try:
+		passo, animacao = lerArquivoAnimacao(nomeArquivo)
+	except Exception, e:
+		traceback.print_exc()
+		print "Informe um nome de arquivo válido."
+		sys.exit(1)
+
+	renderizar(passo, animacao)
 
 if __name__ == '__main__':	
 	main()
