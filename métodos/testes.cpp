71f20aeb429dce0cc29855d32a6610c3c830929c
Daniel Augusto <danisson10@gmail.com>
2015-05-15 14:25:42 -0300

    Começando os testes

---
+++ métodos/testes.cpp
@@ -0,0 +1,22 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch/catch.hpp"
+#include "métodos.h"
+#include <cmath>
+
+
+TEST_CASE("e^-x para x := 6 e step := 1e-4","[euler][backward]") {
+	chp::equacaoDiferencial F = [](double y, double x){return -y;};
+
+	const double step = 1e-4,
+	             epsilon = 1e-1,
+	             x = 6.0;
+
+	const double y = 0.002478752176666,
+	             y0 = 1;
+
+	auto forward = chp::ForwardEuler(F,y0,step);
+	auto backward = chp::BackwardEuler(F,y0,step,epsilon);
+
+	REQUIRE(forward(x) == Approx(y));
+	REQUIRE(backward(x) == Approx(y));
+}
