70ae02bea04fa3d2d4d85ef54a03edf7f21e9781
Daniel Augusto <danisson10@gmail.com>
2015-06-05 22:37:20 -0300

    Mudando pequenas coisas. Opção de sair como a última, limpando a memória usada e alguns ajustes de identação.

--- métodos/main.cpp
+++ métodos/main.cpp
@@ -12,25 +12,24 @@ int main(int argc, char const *argv[])
 {
 	enum Opcao {FORW_EULER, BACK_EULER, EULER_MOD, RUNGE_KUTTA,
 	            RUNGE_KUTTA_3, RUNGE_KUTTA_4, PRED_CORR_3,
-	            PRED_CORR_4, SAIR, ANALISE_GERAL};
+	            PRED_CORR_4, ANALISE_GERAL,SAIR};
 
 	std::string opcaoName[] = {"Forward Euler", "Backward Euler", "Euler Modificado",
 	                           "Runge Kutta Segunda Ordem", "Runge Kutta Terceira Ordem",
 	                           "Runge Kutta Quarta Ordem", "Preditor Corretor Terceira Ordem",
-	                           "Preditor Corretor Quarta Ordem", "Sair", "Análise de todos os métodos"};
-	bool feito = false;
+	                           "Preditor Corretor Quarta Ordem", "Análise de todos os métodos","Sair"};
 	int metodo;
 	const int numMetod = 8; //Número de métodos
 	double step, epsilon;
-	std::array<chp::ResolvedorEDO*, numMetod> resolvedores;
+	std::array<chp::ResolvedorEDO*, numMetod> resolvedores = {nullptr};
 
-	tnw::Vetor valIniciais = {/* y1  = */ 1,
-	                          /* y1' = */ 0,
-	                          /* y2  = */ 1,
-	                          /* y2' = */ 0,
+	tnw::Vetor valIniciais = {/* y1  = */   1 ,
+	                          /* y1' = */   0 ,
+	                          /* y2  = */   1 ,
+	                          /* y2' = */   0 ,
 	                          /* g   = */ 9.81};
 
-	                                 /*          y1, y1',   y2, y2', g */
+	                                 /*         y1 , y1',  y2 , y2', g */
 	tnw::MatrizQuadrada sistemaEq = {/* y1' */{   0,   1,    0,   0, 0},
 	                                 /* y1''*/{-200, -10,  100,   0, 1},
 	                                 /* y2' */{   0,   0,    0,   1, 0},
@@ -38,25 +37,28 @@ int main(int argc, char const *argv[])
 	                                 /* g'  */{   0,   0,    0,   0, 0}};
 	tnw::Vetor resultado;
 
-	while (!feito){
+	while (true){
 		std::cout << "Bem-vindos à simulação de equações diferenciais!" << std::endl << "Selecione o método que você quer executar:" << std::endl;
-		for (int i = FORW_EULER; i <= ANALISE_GERAL; i++){
-			std::cout << "(" << i << ")" << opcaoName[i] << "\n"; 
-		} 	
+		for (int i = FORW_EULER; i <= SAIR; i++)
+			std::cout << "(" << i << ")" << opcaoName[i] << std::endl;
 		std::cin >> metodo;
+
 		if (metodo!=SAIR){
 			std::cout << "Informe o passo do método: ";
 			std::cin >> step;
 			std::cout << "Informe a precisão do ponto fixo: ";
 			std::cin >> epsilon;
-			resolvedores[FORW_EULER] = new chp::ForwardEuler(sistemaEq, valIniciais, step);
-			resolvedores[BACK_EULER] = new chp::BackwardEuler(sistemaEq, valIniciais, step, epsilon);
-			resolvedores[EULER_MOD] = new chp::EulerModificado(sistemaEq, valIniciais, step, epsilon); 
-			resolvedores[RUNGE_KUTTA] = new chp::RungeKutta(sistemaEq, valIniciais, step);
-			resolvedores[RUNGE_KUTTA_3] = new chp::RungeKutta3(sistemaEq, valIniciais, step);
-			resolvedores[RUNGE_KUTTA_4] = new chp::RungeKutta4(sistemaEq, valIniciais, step);
-			resolvedores[PRED_CORR_3] = new chp::PreditorCorretor3(sistemaEq, valIniciais, step);
-			resolvedores[PRED_CORR_4] = new chp::PreditorCorretor4(sistemaEq, valIniciais, step);
+
+			for (chp::ResolvedorEDO* r : resolvedores) delete r;
+
+			resolvedores[FORW_EULER]    = new chp::ForwardEuler      (sistemaEq, valIniciais, step);
+			resolvedores[BACK_EULER]    = new chp::BackwardEuler     (sistemaEq, valIniciais, step, epsilon);
+			resolvedores[EULER_MOD]     = new chp::EulerModificado   (sistemaEq, valIniciais, step, epsilon);
+			resolvedores[RUNGE_KUTTA]   = new chp::RungeKutta        (sistemaEq, valIniciais, step);
+			resolvedores[RUNGE_KUTTA_3] = new chp::RungeKutta3       (sistemaEq, valIniciais, step);
+			resolvedores[RUNGE_KUTTA_4] = new chp::RungeKutta4       (sistemaEq, valIniciais, step);
+			resolvedores[PRED_CORR_3]   = new chp::PreditorCorretor3 (sistemaEq, valIniciais, step);
+			resolvedores[PRED_CORR_4]   = new chp::PreditorCorretor4 (sistemaEq, valIniciais, step);
 		}
 
 		switch (metodo) {
@@ -124,11 +126,6 @@ int main(int argc, char const *argv[])
 				chp::iterarMetodo(*resolvedores[PRED_CORR_4], [&](tnw::Vetor x) {printer.printVetor(x);}, 20);
 				break;
 			}
-			case SAIR: {
-				std::cout << "Tchau!\n";
-				feito = true;
-				break;
-			}
 			case ANALISE_GERAL: {
 				std::cout << "Informe a pasta que ficará os arquivos da análise: ";
 				std::string nomePasta;
@@ -177,6 +174,11 @@ int main(int argc, char const *argv[])
 
 				break;
 			}
+			case SAIR: {
+				std::cout << "Tchau!\n";
+				for (chp::ResolvedorEDO* r : resolvedores) delete r;
+				goto fim;
+			}
 			default: {
 				std::cout << "Opção incorreta!\n";
 				break;
@@ -184,5 +186,6 @@ int main(int argc, char const *argv[])
 		}
 	}
 
+	fim:
 	return 0;
 }
